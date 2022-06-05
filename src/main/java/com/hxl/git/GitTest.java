db19733138cb4d9526d74ae2aab639a0ae236768
hxl <2961539144@qq.com>
2022-06-05 22:27:43 +0800

    first commit

---
+++ src/main/java/com/hxl/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.hxl.git;
+
+/**
+ * @Author hxl
+ * @Date 2022/6/5 22:24
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
